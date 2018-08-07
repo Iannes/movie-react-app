7571b6d10623109e153b11a0e55f97d485a7fc44
Yannis Spyrou <js15js@gmail.com>
2018-08-07 16:02:34 +0100

    fix: Add checks

--- src/App.js
+++ src/App.js
@@ -25,15 +25,20 @@ class App extends Component {
               // create array where we'll store each row
               const movieRows = []
               // map throught results
-              results.map(movie => {
-                 movie.poster_src = `https://image.tmdb.org/t/p/w185${movie.poster_path}`
-                // pass movie props
-                const movieRow = <MovieRow key={movie.id} movie = {movie} />
-                // push into array
-                return movieRows.push(movieRow)
-              })
+              if(results) {
+                results.map(movie => {
+                   movie.poster_src = `https://image.tmdb.org/t/p/w185${movie.poster_path}`
+                  // pass movie props
+                  const movieRow = <MovieRow key={movie.id} movie = {movie} />
+                  // push into array
+                  return movieRows.push(movieRow)
+                })
+              }
+              if(movieRows ) {
+
+                this.setState({ rows: movieRows})
+              }
               // use array to set state
-              this.setState({ rows: movieRows})
             })
             .catch(error => console.warn(error))
   }
